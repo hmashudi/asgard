8d915304f3c42d4d3e848cc025e35af3b9c70c2e
Hofid Mashudi <hofidmashudi@gmail.com>
2019-03-29 18:16:59 +0700

    Refactor alphaclient code
    - set alphaserver IP to send the metric
    - get alphaclient hostname as part of report data
    - use tailer to monitor /var/log/auth.log for incoming ssh login and
    compose the message to report to alphaserver

--- alphaclient/alphaclient.py
+++ alphaclient/alphaclient.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
-from socket import *
-
+import socket
+import tailer
 
 def client_program():
-    host = socket.gethostname()
+    host = '192.168.33.10'
     port = 5000
 
-    client_socket = socket()
+    client_socket = socket.socket()
     client_socket.connect((host, port))
+    hostname = socket.gethostname()
 
-    message = input(" -> ")
-
-    while message.lower().strip() != 'quit':
-        client_socket.send(message.encode())
-        message = input(" -> ")
+    init_attempt = 0
+    for line in tailer.follow(open('/var/log/auth.log')):
+        if "sshd" and "session opened" in line:
+            login_attempt = init_attempt + 1
+            message = (str(hostname) + " had " + str(login_attempt) + " attempt of ssh session")
+            client_socket.send(message.encode())
+            init_attempt = login_attempt
 
     client_socket.close()
 
